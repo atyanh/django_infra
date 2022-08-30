7089c0725733744dc8ae375ca339fdbff97c8126
atyanh <atyanh1999@gmail.com>
2022-08-30 12:27:35 +0400

    Testing jenkins

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -4,11 +4,13 @@ pipeline {
         stage('pull update') {
             steps {
                 sh 'echo Pulling updates..'
+                sh 'git clone https://github.com/atyanh/django.git'
             }
         }
 	stage('build') {
             steps {
                 sh 'echo building..'
+                sh 'ls'
             }
         }
 	stage('update') {
