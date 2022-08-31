49752c6854beb673b8a2683b74041184ec20af0c
atyanh <atyanh1999@gmail.com>
2022-08-31 13:43:22 +0400

    test jenkins

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -14,14 +14,14 @@ pipeline {
     stage('Building image') {
       steps{
         script {
-          dockerImage = docker.build $registry + ":$BUILD_NUMBER"
+          dockerImage = docker.build registry + ":$BUILD_NUMBER"
         }
       }
     }
     stage('Deploy Image') {
       steps{
         script {
-          docker.withRegistry( $registry, registryCredential ) {
+          docker.withRegistry( registry, registryCredential ) {
             dockerImage.push()
           }
         }
