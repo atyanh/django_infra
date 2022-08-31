56adbac16633c0f005b22e27499b13848ae7f168
atyanh <atyanh1999@gmail.com>
2022-08-31 13:04:21 +0400

    test jenkins

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -1,33 +1,3 @@
-// pipeline {
-//     agent any
-//     stages {
-//         stage('pull update') {
-//             steps {
-//                 sh 'rm -rf django/'
-//                 sh 'echo Pulling updates..'
-//                 sh 'git clone https://github.com/atyanh/django.git'
-//             }
-//         }
-// 	    stage('build') {
-//             steps {
-//                 sh 'docker rmi localhost:8082/repository/django/web:latest'
-//                 sh 'docker login localhost:8082 -u admin -p admin123'
-//                 sh 'docker build ./django/ -t localhost:8082/repository/django/web:latest'
-//                 sh 'ls'
-//             }
-//         }
-// 	    stage('push to repository') {
-//             steps {
-//                 sh 'docker push localhost:8082/repository/django/web:latest'
-//             }
-//         }
-    
-
-//     }
-// }
-
-
-
 pipeline {
   environment {
     registry = "localhost:8082/repository/django"
@@ -64,3 +34,4 @@ pipeline {
     }
   }
 }
+
