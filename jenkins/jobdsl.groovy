232b4e464d013fba84bc1912f7b0bcf61255f198
atyanh <atyanh1999@gmail.com>
2022-09-01 17:11:42 +0400

    Done Something

---
+++ jenkins/jobdsl.groovy
@@ -0,0 +1,20 @@
+pipelineJob('ci_cd_pipeline') {
+    triggers {
+              scm('H/5 * * * *')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('atyanh/django_infra')
+                    }
+                }
+            }
+            scriptPath('jenkins/pipeline.groovy')
+        }
+    }
+    properties {
+        githubProjectUrl('https://github.com/atyanh/django')
+    }
+}
