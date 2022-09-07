8c97ee32b1cf5c288e459752e831e26d8c0b689c
atyanh <93996923+atyanh@users.noreply.github.com>
2022-09-07 14:59:17 +0400

    Update pipeline.groovy

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -14,18 +14,31 @@ pipeline {
       steps{
         script {
           sh "docker build . -t $registry:$BUILD_NUMBER"
+          sh "docker build . -t $registry:latest"
         }
       }
     }
-    stage('Deploy Image') {
+    stage('Push Image') {
       steps{
-        sh "docker login localhost:8082 -u admin -p $nexus_password && docker push $registry:$BUILD_NUMBER"
+        sh "docker login localhost:8082 -u admin -p $nexus_password && docker push $registry:$BUILD_NUMBER && docker push $registry:latest"
       }
     }
 
     stage('Remove Unused docker image') {
       steps{
-        sh "docker rmi $registry:$BUILD_NUMBER"
+        sh "docker rmi $registry:$BUILD_NUMBER && docker rmi $registry:latest"
+      }
+    }
+
+    stage('Pushing Image') {
+      steps{
+        sh "docker login localhost:8082 -u admin -p $nexus_password && docker push $registry:$BUILD_NUMBER"
+      }
+    }
+
+    stage('Deploy on EKS') {
+      steps {
+        sh "docker run  -e 'AWS_ACCESS_KEY_ID=AKIAVPVJ5V2ZA5KKRA22' -e 'AWS_SECRET_ACCESS_KEY=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' -v /home/ubuntu/django_web.yml:/root/django_web.yml --rm bearengineer/awscli-kubectl /bin/sh -c '/usr/bin/aws eks update-kubeconfig --region us-east-1 --name eks_cluster && kubectl apply -f /root/django_web.yml'"                           
       }
     }
   }
