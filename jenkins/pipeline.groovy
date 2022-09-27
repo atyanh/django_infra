316621d25032617f714f989d652d442bd2f845fa
atyanh <93996923+atyanh@users.noreply.github.com>
2022-09-27 13:13:01 +0400

    Update pipeline.groovy

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -2,6 +2,8 @@ pipeline {
   environment {
     registry = "localhost:8082/repository/django/web"
     nexus_password = credentials('nexus_password')
+    aws_access_key_id = credentials('aws_access_key_id')
+    aws_secret_access_key = credentials('aws_secret_access_id')
   }
   agent any
   stages {
@@ -38,7 +40,7 @@ pipeline {
 
     stage('Deploy on EKS') {
       steps {
-        sh "docker run  -e 'AWS_ACCESS_KEY_ID=AKIAVPVJ5V2ZA5KKRA22' -e 'AWS_SECRET_ACCESS_KEY=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' -v /home/ubuntu/django_web.yml:/root/django_web.yml --rm bearengineer/awscli-kubectl /bin/sh -c '/usr/bin/aws eks update-kubeconfig --region us-east-1 --name eks_cluster && kubectl apply -f /root/django_web.yml'"                           
+        sh "docker run  -e AWS_ACCESS_KEY_ID=$aws_access_key_id -e AWS_SECRET_ACCESS_KEY=$aws_secret_access_key -v /home/ubuntu/django_web.yml:/root/django_web.yml --rm bearengineer/awscli-kubectl /bin/sh -c '/usr/bin/aws eks update-kubeconfig --region us-east-1 --name eks_cluster && kubectl apply -f /root/django_web.yml'"                           
       }
     }
   }
